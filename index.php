ab781cc6fbac45474d2ce7a0a6219e59831a1114
Jordan Wellington Lint <eyeoh3@Juanma.local>
2015-03-11 12:48:15 +0100

    Add SVG whip

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+$params   = $_SERVER['QUERY_STRING'];
+$query = $params;
+
+foreach (explode('&', $query) as $chunk) {
+    $param = explode("=", $chunk);
+
+    if ($param) {
+        printf("Value for parameter \"%s\" is \"%s\"<br/>\n", urldecode($param[0]), urldecode($param[1]));
+    }
+}
+
+//
+
+$hexadecimal = 'A37334';
+echo base_convert($hexadecimal, 16, 2);
+
+//
+
+function alpha2num($a) {
+    $r = 0;
+    $l = strlen($a);
+    for ($i = 0; $i < $l; $i++) {
+        $r += pow(26, $i) * (ord($a[$l - $i - 1]) - 0x40);
+    }
+    return $r - 1;
+}
+
+echo alpha2num('hello');
+
+?>
