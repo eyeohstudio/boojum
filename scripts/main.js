7fcdfba7bc881df1f64314eca2cdfbac108640a5
Jordan Wellington Lint <hello@jasonlint.net>
2015-04-13 16:35:47 +0200

    Fix getDate again

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function ($) {
   
   //
   var dateNow = new Date();
-  var dateToday = ( '0' + (dateNow.getMonth()+1) ).slice( -2 ) + ( '0' + (dateNow.getDate()) );
+  var dateToday = ( '0' + (dateNow.getMonth()+1) ).slice( -2 ) + ( '0' + dateNow.getDate()).slice(-2);
   var dateCurrent = getUrlParameter('date') || dateToday;
   var date = parseInt(dateCurrent, 10);
   
